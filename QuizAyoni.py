6279a1878a5b17e925eabc556ed753720e288593
MissHarah <121475253+MissHarah@users.noreply.github.com>
2023-01-20 08:58:01 +0100

    Add files via upload

---
+++ QuizAyoni.py
@@ -0,0 +1,15 @@
+print("Asalamualykum warohmatullah")
+
+Quiz=input("who was the first sahaba to make adhan ")
+
+if Quiz=="Billal":
+    
+      print("correct answer")
+
+elif Quiz=="abdulrahman ibn Hamzah":
+
+      print("you are almost there!!!")
+
+else:
+     print ("quiz over!!!")
+     
