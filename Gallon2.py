e6e68d5f2b78b637d0f98f778cb3038d1e8113c0
MissHarah <motunrayor2@gmail.com>
2023-01-20 08:49:54 +0100

    first commit

---
+++ Gallon2.py
@@ -0,0 +1,10 @@
+
+volume_of_rain=input("Enter volume of rainfall in inches: ")
+volume_of_acres=input("Enter size of Acres: ")
+
+
+
+gallon = (int(volume_of_rain) /231)  + int(volume_of_acres *2)
+
+print(" there are ",gallon ,"of water", "accumated on acres of land")
+
