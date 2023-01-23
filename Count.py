8e0a0bee9c5967880834307b9bf725def904fdd6
MissHarah <121475253+MissHarah@users.noreply.github.com>
2023-01-23 09:03:29 +0100

    Add files via upload

---
+++ Count.py
@@ -0,0 +1,17 @@
+print("Asalamualykum warohmatullah")
+count = 0
+while count < 20:
+    Quiz=input("who was the first sahaba to make adhan ")
+    print(count)
+    count += 1    
+    if Quiz=="Billal":
+        print("correct answer")
+    if Quiz=="Abdulrahman ibn Hamzah":
+        print("you are almost there!!!")
+    break
+    Quiz=input("who is the foster father of the prophet")
+    print(Count)
+    count +=1
+    if Quiz=="Abdullah"
+        print ("correct")
+  
